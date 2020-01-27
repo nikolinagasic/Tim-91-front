825f118fc69735808c62d38762a2b9975b3ee5bb
abeker <acabeker@gmail.com>
2020-01-27 23:27:18 +0100

    implemented history

--- src/Content/mainPage/PatientRate.js
+++ src/Content/mainPage/PatientRate.js
@@ -88,31 +88,34 @@ class PatientRate extends React.Component {
         return (
             <div id="a_rating_main_div">
                 <div id="a_sort_table_of_examination">
-                    <div className="a_date_examination_inline">
-                        <p>Датум прегледа:</p>
-                        <input type="date"
-                            min="2019-12-20" max="2020-02-12"
-                            id="a_date_table_of_examination"
-                            onChange={this.sort}></input>
-                    </div>
-                    <div className="a_date_examination_inline">
-                        <p>Тип прегледа:</p>
-                        <select className="a_select_examination" 
-                            id="a_select_table_of_examination"
-                            onChange={this.sort}>
-                            {this.generateOptions(this.state.options)}
-                        </select>
-                    </div>
-                    <div className="a_date_examination_inline">
-                        <p>Врста термина:</p>
-                        <select className="a_select_examination" 
-                            id="a_selectVrsta_table_of_examination"
-                            onChange={this.sort}>
-                            <option>Прегледи и операције</option>
-                            <option>Преглед</option>
-                            <option>Операција</option>
-                        </select>
-                    </div>
+                    <h1><u>Историја прегледа и операција</u></h1>
+                    <form class="a_patientRate_form-inline">
+                        <div>
+                            <p>Датум прегледа:</p>
+                            <input type="date"
+                                min="2019-12-20" max="2020-02-12"
+                                id="a_date_table_of_examination"
+                                onChange={this.sort}></input>
+                        </div>
+                        <div>
+                            <p>Тип прегледа:</p>
+                            <select className="a_select_examination" 
+                                id="a_select_table_of_examination"
+                                onChange={this.sort}>
+                                {this.generateOptions(this.state.options)}
+                            </select>
+                        </div>
+                        <div>
+                            <p>Врста термина:</p>
+                            <select className="a_select_examination" 
+                                id="a_selectVrsta_table_of_examination"
+                                onChange={this.sort}>
+                                <option>Прегледи и операције</option>
+                                <option>Преглед</option>
+                                <option>Операција</option>
+                            </select>
+                        </div>
+                    </form>
                 </div>
         
                 <table className="a_rating_table_of_examinations">
